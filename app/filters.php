0245a105533475ddf4aa891000f4df2963d88721
clusteramaryllis <clusteramaryllis77@gmail.com>
2014-04-29 19:15:18 +0700

    Revisi

--- app/filters.php
+++ app/filters.php
@@ -143,17 +143,3 @@ if (!function_exists('rand_without'))
         return $number;
     }    
 }
-
-function formatOffset($offset) {
-    $hours = $offset / 3600;
-    $remainder = $offset % 3600;
-    $sign = $hours > 0 ? '+' : '-';
-    $hour = (int) abs($hours);
-    $minutes = (int) abs($remainder / 60);
-
-    if ($hour == 0 AND $minutes == 0) {
-        $sign = ' ';
-    }
-    return 'GMT' . $sign . str_pad($hour, 2, '0', STR_PAD_LEFT) 
-            .':'. str_pad($minutes,2, '0');
-}
